78d35dbeae2a7f0e9009db0283160e34cd030bcb
Leandro Souza dos Santos <l.souzadsantos@gmail.com>
2021-04-22 22:55:39 -0300

    Add DateTime extensions

---
+++ src/CommonExtensionsDotNet/DateTimeExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace CommonExtensionsDotNet
+{
+    public static class DateTimeExtension
+    {
+        public static bool IsBusinessDay(this DateTime dateTime)
+        {
+            return !IsWeekend(dateTime);
+        }
+
+        public static bool IsWeekend(this DateTime dateTime)
+        {
+            return new[] { DayOfWeek.Saturday, DayOfWeek.Sunday }.Contains(dateTime.DayOfWeek);
+        }
+    }
+}
---
+++ tests/CommonExtensionsDotnet.Tests/DateTimeExtensionTests.cs
@@ -0,0 +1,37 @@
+﻿using CommonExtensionsDotNet;
+using System;
+using Xunit;
+
+namespace CommonExtensionsDotnet.Tests
+{
+    public class DateTimeExtensionTests
+    {
+        [Fact]
+        public void ShouldValidateAsBusinessDay()
+        {
+            var date = DateTime.Now;
+
+            if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                int daysUntilMonday = ((int)DayOfWeek.Monday - (int)date.DayOfWeek + 7) % 7;
+                date.AddDays(daysUntilMonday);
+            }
+
+            var isBusinessDay = date.IsBusinessDay();
+
+            Assert.True(isBusinessDay);
+        }
+
+        [Fact]
+        public void ShouldValidateAsWeekend()
+        {
+            var today = DateTime.Today;
+            int daysUntilSaturday = ((int)DayOfWeek.Saturday - (int)today.DayOfWeek + 7) % 7;
+            var saturday = today.AddDays(daysUntilSaturday);
+
+            var isWeekend = saturday.IsWeekend();
+
+            Assert.True(isWeekend);
+        }
+    }
+}
